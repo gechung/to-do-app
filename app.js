0933f24f68da35cc0bd62096763d3c8804c18fae
Grace <gechung@gmail.com>
2019-07-12 13:42:47 -0700

    Assignment: completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-  addToDoForm.addEventListener('submit', event => {
+  addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
 
     // get the text
@@ -15,7 +15,10 @@ function onReady() {
     let checkbox = document.createElement('input');
 
     // set the input's type to checkbox
-    checkbox.type = "checkbox";
+    checkbox.type = 'checkbox';
+
+    let minusBtn = document.createElement('button');
+    minusBtn.innerHTML = '<span>Delete</span>';
 
     // set the title
     newLi.textContent = title;
@@ -23,14 +26,22 @@ function onReady() {
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    // listener for minus button to be clicked when removing item
+    newLi.appendChild(minusBtn);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     // empty the input
     newToDoText.value = '';
 
-  });
-}
+  minusBtn.addEventListener('click', function() {
+    newLi.parentNode.removeChild (newLi);
+  })
+  console.log (title);
+});
+
+};
 
 window.onload = function() {
   onReady();
