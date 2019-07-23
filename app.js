14f59167663f852036638bb3e371db1a5a145bbc
Grace <gechung@gmail.com>
2019-07-23 12:25:51 -0700

    Assignment: draft

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 function onReady() {
   const toDos = [];
   const addToDoForm = document.getElementById ('addToDoForm');
-
-  function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
+  let id = 0;
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
@@ -26,11 +12,41 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "Delete";
+
+      deleteBtn.addEventListener('click', event => {
+      toDos = toDos.filter(function(item){
+        return item.id != toDo.id;
+        });
+
+      renderTheUI();
+
+    });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
     });
+  }
+
+  function createNewToDo() {
+    let id = 0;
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id
+    });
+
+    id++;
+
+    newToDoText.value = '';
+    renderTheUI();
   }
 
   addToDoForm.addEventListener('submit', event => {
